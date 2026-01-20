0c7f7680a10e1932e4adc998cf77819c97772dd4
Maciej <maciej.polowy1@gmail.com>
2026-01-20 21:56:21 +0100

    fix google verification

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,10 +1,6 @@
 export default function Head() {
   return (
     <>
-      <meta
-        name="google-site-verification"
-        content="QcsIwqa5MvuvVn6iTG0cGpAfQxTdY9dtnRk3N3oGis4"
-      />
       <link rel="preload" href="/icons/ffmpeg.svg" as="image" />
       <link rel="preload" href="/icons/gsap.svg" as="image" />
       <link rel="preload" href="/icons/nextjs.svg" as="image" />
