4ff18d9ea57f25f5e3dc8d51958a2235808478db
Maciej <maciej.polowy1@gmail.com>
2026-02-23 12:11:26 +0100

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/layout/footer";
 import Navbar from "@/components/layout/navbar";
 import About from "@/components/sections/about/about";
 import Contact from "@/components/sections/contact/contact";
-import Experience from "@/components/sections/experience/experience";
 import Hero from "@/components/sections/hero/hero";
 import Projects from "@/components/sections/projects/projects";
 import Techstack from "@/components/sections/techstack/techstack";
@@ -16,7 +15,7 @@ export default function HomePage() {
       <Techstack />
       <About />
       <Projects />
-      <Experience />
+      {/* <Experience /> */}
       <WebsiteTechnologies />
       <Contact />
       <Footer />
